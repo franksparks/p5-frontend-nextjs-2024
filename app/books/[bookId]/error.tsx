523c386e7fc3f4e4d39aa75f6ee5e78e3c10636d
Ferran Bals - iMac <ferranbals@gmail.com>
2024-06-09 13:08:28 +0200

    Added link on error page

--- app/books/[bookId]/error.tsx
+++ app/books/[bookId]/error.tsx
@@ -1,11 +1,23 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 type ErrorProps = {
   error: Error & { digest?: string };
   reset: () => void;
 };
 export default function Error({ error, reset }: ErrorProps) {
   return (
-    <div className="p-4 bg-red-500 text-white">Error: {error.message}</div>
+    <div>
+      <div className="p-4 bg-red-500 text-white">
+        Error: {error.message}
+      </div>
+      <Button className="ml-10 mt-10">
+        <Link href="/books" className="text-white ">
+          &#8617; Volver al listado
+        </Link>
+      </Button>
+    </div>
   );
 }
